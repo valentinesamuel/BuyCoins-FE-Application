23e4168630a156b333393b9637b3b390cca66d0a
Samuel Valentine <valentinesamuel2580@gmail.com>
2021-05-22 09:34:57 +0100

    (FEAT): Made the nav-bar responsive and fully functional

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,1 +1,8 @@
-console.log("Hooray, i am in the profile page");
+const mobMenu = document.getElementById("mobile-menu");
+const menu = document.getElementById("menu");
+
+mobMenu.addEventListener('click', e => {
+      console.log('mobile');
+      // menu.style.visibility="visible"
+      document.querySelector('#menu').classList.toggle('active')
+})
