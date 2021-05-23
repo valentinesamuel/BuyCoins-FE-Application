13cf7b886c4f1521e2a1640c98c16f1ee5a8dc21
valentinesamuel <valentinesamuel2580@gmail.com>
2021-05-23 03:21:23 -0700

    (FEAT) Finished the dummy UI, Time to pull the data and populate the fields

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,8 +1,33 @@
 const mobMenu = document.getElementById("mobile-menu");
 const menu = document.getElementById("menu");
+// const repo = document.getElementById("repository");
+const repoList = document.getElementById("repo-list");
 
 mobMenu.addEventListener('click', e => {
       console.log('mobile');
       // menu.style.visibility="visible"
-      document.querySelector('#menu').classList.toggle('active')
+      document.querySelector('#menu').classList.toggle('active');
 })
+const repoNames = ['👩', '👨', '🧑', '👧','👦', '👶','🧒', '👵','👴', '🧓','👩‍🦰', '👨‍🦰','👩‍🦱', '👨‍🦱','👨‍🦲','👳‍♂️','🙆‍♀️','🙆‍♂️','🧏‍♀️','🧏‍♂️','💁‍♀️','💁‍♂️','🙋‍♀️','🙋‍♂️']
+
+setTimeout(() => {
+    repoNames.forEach(eleme => {
+      const repo = document.createElement('div')
+      repo.classList.add('repository') 
+      const repoMarkup = `<div class="repo-desc">
+              <h3 class="repo-name"><a href="#">${eleme}</a></h3>
+              <span>
+                <div class="lng-color"></div>
+                <span class="lng-name">TypeScript</span>
+                <span class="last-updated">Updated 3 days ago</span>
+              </span>
+            </div>
+            <div class="repo-status">
+              <button>Star</button>
+            </div>`
+
+      repo.innerHTML = "";
+      repo.innerHTML = repoMarkup;
+      repoList.appendChild(repo);
+    })
+}, 2000);
